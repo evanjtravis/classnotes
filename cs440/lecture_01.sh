25961b1046e76947bdce9119cc8a48dec163146a
Evan J. Travis <ejtravis@illinois.edu>
2015-01-25 12:43:08 -0600

    added/touched beginning files. Notes have .sh extension to support iinstances of syntax hilighting.

---
+++ cs440/lecture_01.sh
@@ -0,0 +1,103 @@
+What is AI?
+    # Think like a human
+    # Act like a human
+    # Think rationally
+    # Act Rationally
+
+#####################################################################
+# Thinking Humanly
+#####################################################################
+    Study brain structure, cognitive science and neuroscience
+    Some projects build computers modeled after brains:
+        # Brain Initiative
+        # Human Brain Project
+
+#####################################################################
+# Acting Humanly
+#####################################################################
+    Turing Test
+        # A computer needs THESE for successful turing test:
+            Natural language processing
+            Knowledge Representation
+            Automated Reasoning
+            Machine Learning
+    # Loebner Prize
+    # Eugene Goostman
+        June 2014, passed Turing Test
+    
+    # Turing Test Criticisms
+        Variability in protocols and judges
+        Success depends on deception
+        chatbots can do well using "cheap tricks"
+            "Changing the conversation"
+            "echoing back lines like a psychologist"
+                # Eliza (1966)
+        # Chinese Room Argument
+            It is possible to have the appearence of intelligence without being intelligent (philosophical criticism).
+    A Better Turing Test?
+        ' A multiple choice test consisting of questions easily answered
+        by humans, but that cannot be answerd by machines using 'cheap tricks''
+        Example
+            The trophy would not fit in the brown suitcase beacuase it was so small. What was so small?
+                (a) The suitcase?
+                (b) The trophy?
+        # Advantages:
+            Tests can be administered and graded by a computer
+            Does not depend on human subjectivity
+            Does not require the ability to generate english sentences
+            Questions cannot be evaded using verbal dodges: "cheap tricks"
+            questions can be made "google-proof"
+
+#####################################################################
+# Thinking Rationally
+#####################################################################
+    Idealized or "right" way of thinking
+    # Logic
+        Patterns of argument that always yield correct conclusions when supplied with correct premises
+    # Logicist approach to AI
+        Describe the problem in formal logical notation and apply general deductin procedures to solve it.
+    # Problems with Logicist approach
+        Computational complexity in finding a solution
+        Describing real-world problems and knowledge in logical notation
+        Dealing with uncertainty
+        A lot of "rational" behavior has noting to do with logic
+            reflexes
+            folkways
+            some mores
+'
+#####################################################################
+# Acting Rationally ---> THE FOCUS FOR THIS COURSE
+#####################################################################'
+    A "Rational Agent" acts to optimally acheive goals
+        Goals are application dependent and expressed in terms of the "utility of outcomes"
+        Being Rational means "Maximizing your (expected) utility"
+    This definition of rationality only concerns the decisions/actions that are made/executed , !'NOT'! the underlying cognitive processes involved.
+    In practice, utility optimization is subject to the agent''s computational constraints
+        # Bounded rationality
+            or
+        # Bounded optimality
+    
+    # Utility Maximization Formula
+        Advantages
+            Generality:
+                goes beyond explicit reasoning and even human congnition altogether
+            Practicality:
+                can be adapated to many real world problems
+            Naturally accomodates uncertainty
+            Amenable to good scientific and engineering methodology
+            Avoids philosophy and psychology
+        Disadvantages
+            Difficult to formulate utility functions
+                How to be a good chatbot?
+                How to autopilot a helicopter?
+            leaving off important variables in utility function
+                solving food distribution:
+                    is it efficient?
+                    is it fair?
+                    is it safe?
+            Limited applicability to humans
+                "Humans aren't all rational" - Lazebnik
+                Humans are loss averse
+                    "I will give you 10 dollars but take away 5" <
+                    "I will give you 5 dollars."
+
