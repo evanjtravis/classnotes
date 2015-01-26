ec21bed2dcd270f4f3a471ca01dfeaac02a256c2
Evan J. Travis <ejtravis@illinois.edu>
2015-01-25 23:18:19 -0600

    Improved template. Improved marking of vocab. NOTE TO SELF: provide  standard notation used as file.

--- cs440/lecture_01.sh
+++ cs440/lecture_01.sh
@@ -4,40 +4,40 @@ notes:
     # Intro
     #####################################################################
     What is AI?
-        # Think like a human
-        # Act like a human
-        # Think rationally
-        # Act Rationally
+        """Think like a human"""
+        """Act like a human"""
+        """Think rationally"""
+        """Act Rationally"""
     
     #####################################################################
     # Thinking Humanly
     #####################################################################
     Study brain structure, cognitive science and neuroscience
     Some projects build computers modeled after brains:
-        # Brain Initiative
-        # Human Brain Project
+        """Brain Initiative"""
+        """Human Brain Project"""
     
     #####################################################################
     # Acting Humanly
     #####################################################################
     Turing Test
-        # A computer needs THESE for successful turing test:
+        A computer needs """THESE""" for successful turing test:
             Natural language processing
             Knowledge Representation
             Automated Reasoning
             Machine Learning
-    # Loebner Prize
-    # Eugene Goostman
-        June 2014, passed Turing Test
+    """Loebner Prize"""
+    """Eugene Goostman"""
+        June (2014), passed Turing Test
     
-    # Turing Test Criticisms
-        Variability in protocols and judges
+    Turing Test """Criticisms"""
+       Variability in protocols and judges
         Success depends on deception
         chatbots can do well using "cheap tricks"
             "Changing the conversation"
             "echoing back lines like a psychologist"
-                # Eliza (1966)
-        # Chinese Room Argument
+                """Eliza""" (1966)
+        """Chinese Room Argument"""
             It is possible to have the appearence of intelligence without being intelligent (philosophical criticism).
     A Better Turing Test?
         ' A multiple choice test consisting of questions easily answered
@@ -46,7 +46,7 @@ notes:
             The trophy would not fit in the brown suitcase beacuase it was so small. What was so small?
                 (a) The suitcase?
                 (b) The trophy?
-        # Advantages:
+        """Advantages:"""
             Tests can be administered and graded by a computer
             Does not depend on human subjectivity
             Does not require the ability to generate english sentences
@@ -57,11 +57,11 @@ notes:
     # Thinking Rationally
     #####################################################################
     Idealized or "right" way of thinking
-    # Logic
+    """Logic"""
         Patterns of argument that always yield correct conclusions when supplied with correct premises
-    # Logicist approach to AI
+    """Logicist approach to AI"""
         Describe the problem in formal logical notation and apply general deductin procedures to solve it.
-    # Problems with Logicist approach
+    """Problems with Logicist approach"""
         Computational complexity in finding a solution
         Describing real-world problems and knowledge in logical notation
         Dealing with uncertainty
@@ -73,17 +73,17 @@ notes:
     #####################################################################
     # Acting Rationally ---> THE FOCUS FOR THIS COURSE
     #####################################################################'
-    A "Rational Agent" acts to optimally acheive goals
-    Goals are application dependent and expressed in terms of the "utility of outcomes"
-    Being Rational means "Maximizing your (expected) utility"
+    A """Rational Agent""" acts to optimally acheive goals
+    Goals are application dependent and expressed in terms of the """utility of outcomes"""
+    Being Rational means """Maximizing your (expected) utility"""
     This definition of rationality only concerns the decisions/actions that are made/executed , !'NOT'! the underlying cognitive processes involved.
     In practice, utility optimization is subject to the agent''s computational constraints
-        # Bounded rationality
+        """Bounded rationality"""
             or
-        # Bounded optimality
+        """Bounded optimality"""
     
-    # Utility Maximization Formula
-        Advantages
+    """Utility Maximization Formula"""
+        """Advantages"""
             Generality:
                 goes beyond explicit reasoning and even human congnition altogether
             Practicality:
@@ -91,7 +91,7 @@ notes:
             Naturally accomodates uncertainty
             Amenable to good scientific and engineering methodology
             Avoids philosophy and psychology
-        Disadvantages
+        """Disadvantages"""
             Difficult to formulate utility functions
                 How to be a good chatbot?
                 How to autopilot a helicopter?
@@ -134,10 +134,16 @@ Utility Maximization.Advantages:
 Utility Maximization.Disadvantages:
 Bounded Rationality:
 Bounded Optimality:
+Natural language processing:
+Knowledge Representation:
+Automated Reasoning:
+Machine Learning:
 
 ;`
 #####################################################################
 # Timeline
 ;#####################################################################`
 [TIMELINE]
-Eliza=1966
+FORMAT=dd.mm.yyy
+Eliza=00.00.1966
+Eugene Goostman=00.06.2014
--- cs440/lecture_02.sh
+++ cs440/lecture_02.sh
@@ -3,7 +3,56 @@
 # Notes
 ;#####################################################################`
 [NOTES]
-notes:
+notes: AI, History and Themes
+    #################################################################
+    What are some successes of AI today?
+    #################################################################
+    IBM Watson and "Cognitive Computing"
+        (2010) NY Times Article # TODO read Times article
+        Feb (2011) IBM Waston wins on Jeopardy
+        Recently:
+            More links on ppt. # TODO
+    Self-Driving Cars
+    Speech and Natural Language
+    Computer Vision
+        """Deep Learning"""
+    Games
+        """Deep Blue"""
+            (1997) Defeated world champion, Kasparov, in chess
+            (1996) "I could feel -- I could smell -- a new kind of intelligence across the table." - Kasparov
+            (1997) After losing, "Deep Blue hasn't proven anything." - Kasparov
+        (2007) Checkers is solved
+            Checkers programs have been beating humans for years, but now computers could guarantee a win.
+        (2014) Heads-up limit Texas Hold-em poker is solved
+            First game with imperfect information.
+    Mathematics
+        (1996) Argonne National Laboratory proves mathematical conjecture
+            NY Times story: "Solution would be called 'creative' if a human had thought of it."
+        Mathematical Software
+            Wolfram Mathematica
+    Logistics, Scheduling, Planning
+        (1991) Gulf war, AI logistical planning program: 50,000 items to organize, recouped entire AI investment up to that point.
+        (1999) NASA''s """Remote Agent""" operated Deep Space 1 during 2 experiments
+        (2004) NASA introduces """MAPGEN""" --> plans daily operations for Mars Exploration Rovers
+    Robotics
+        Mars Rovers
+        Autonomous vehicles
+            """DARPA Grand Challenge"""
+            Self-driving cars
+        Robot Soccer
+            """Robot Cup"""
+        Personal Robotics
+            Humanoid Robots
+            Robotic pets
+                improvments in elderly lives
+            Personal Assistants (?)
+        Towel Folding Robot
+    #################################################################
+    # Origins of early AI excitement.
+    #################################################################
+    (1940 s)
+        First model of a neuron W.S. @(McCulloch & W. Pitts)
+    (1950 s) Turing Test
 
 `
 #####################################################################
@@ -16,3 +65,4 @@ notes:
 # Timeline
 ;#####################################################################`
 [TIMELINE]
+FORMAT=dd.mm.yyyy
