ffdf9d66ed2f1440e7240ae32717047254ac471c
Evan J. Travis <ejtravis@illinois.edu>
2015-02-02 16:22:48 -0600

    improved configuration and naming conventions.

--- scripts/new_lecture.py
+++ scripts/new_lecture.py
@@ -6,3 +6,14 @@ from ConfigParser import ConfigParser
 parser = ConfigParser()
 parser.read('config/classNotes.config')
 
+
+# look for files with lec.## naming convention in pwd
+# find file w/ largest suffix number
+# copy over template
+
+def main():
+    pass    
+
+
+if __name__ == '__main__':
+    main()
