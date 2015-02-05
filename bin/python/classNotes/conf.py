bf567d3c71f8403dbbfddd30cac6378209adc5ed
Evan J. Travis <ejtravis@illinois.edu>
2015-02-04 19:54:23 -0600

    Determining Base directory is automatic and is replaced in the config file if changed.

--- bin/python/classNotes/conf.py
+++ bin/python/classNotes/conf.py
@@ -3,17 +3,40 @@
 from ConfigParser import ConfigParser
 import os
 
+def get_base_dir():
+    """Determines the base directory of this project based on
+    the absolute path of this program's file and the location of
+    the classNotes directory.
+    """
+    base_dir = '/'
+    root = 'classNotes'
+    this_path = os.path.abspath(__file__)
+    if root in this_path:
+        for directory in this_path.split(os.sep):
+            base_dir = os.path.join(base_dir, directory)
+            if directory == root:
+                return base_dir
+    else:
+        raise Exception(
+            "'%s' is not on the absolute path '%s' of this conf file." \
+            %(root, this_path))
 
-_BASE = '/home/ejtravis/classNotes'
+
+_BASE = get_base_dir()
 CONFIG_FILE = os.path.join(_BASE, 'config/classNotes.config')
 _PARSER = ConfigParser()
 _PARSER.read(CONFIG_FILE)
+_DIRS_SECTION = 'DIRS'
+_TMP_BASE = _PARSER.get(_DIRS_SECTION, 'BASE')
+if _TMP_BASE != _BASE:
+    _PARSER.set('DIRS', 'BASE', _BASE)
+    # Eventually log this change
 
 class AttriDict(dict):
     """A dict whose items can also be accessed as member variables.
     When assigning a variable, checks to make sure that a member of
-    the dictionary's dir() is not being overwritten. An error is
-    thrown when this occurs.
+    the dictionary's read-only dir() is not being overwritten. An
+    error occurs otherwise.
     """
     _INITIALIZE = True
     def __init__(self, *args, **kwargs):
@@ -41,16 +64,19 @@ class AttriDict(dict):
                     "'AttriDict' object attribute '%s' is read-only" \
                     %(attr))
             elif '_' == attr[0]:
-                raise Exception("'%s' is an illegal assignment." %(attr))
+                raise AttributeError(
+                    "'%s' is an illegal attribute assignment.\
+                    \n\t\tRemove underscore(s)." %(attr))
             else:
                 return False
 
-settings = AttriDict() 
-META = 'META'
 
-IGNORE = _PARSER.get(META, 'IGNORE')
+settings = AttriDict() 
+_META = 'META'
+
+_IGNORE = _PARSER.get(_META, 'IGNORE')
 for section_name in _PARSER.sections():
-    if (section_name[:len(IGNORE)] == IGNORE) or (section_name == META):
+    if (section_name[:len(_IGNORE)] == _IGNORE) or (section_name == _META):
         continue
     settings[section_name] = dict(_PARSER.items(section_name))
 
