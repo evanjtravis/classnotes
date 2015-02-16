e7ffd74aa9476242dea079f5a812db31ff57829f
Evan J. Travis <ejtravis@illinois.edu>
2015-02-16 16:47:53 -0600

    Alphabetized functions. Added TODOs.

--- courses/cs440/hw_1/basic_pathfinding.py
+++ courses/cs440/hw_1/basic_pathfinding.py
@@ -4,13 +4,10 @@ import copy
 # Possibly configurable value
 SHOW_VISITED = False
 
-# TODO move manhattan distance algorithm to State class
-# TODO expand state space once but keep track of old file read in with
-# private variable
-# TODO branch, see if still works when checking visited nodes, check Node to see if it has been expanded vs. its state exists in visited states. If it still works, get rid of visited_states list possibly.
 # TODO reorder functions within classes to be alphabetical/priavte vs. public
 # TODO add configuration
 # TODO add command-line arguments (don't use optparse, just iterate through sys.argv)
+# TODO heapq
 class State():
     """This class represents a state within a given state space.
     Each state is comprised of 2 attributes:
@@ -28,13 +25,15 @@ class State():
     def manhattan_distance_from(self, state):
         """Given a State, determines the manhattan distance from
         that State.
+        If the state argument is a Node, function will resolve the
+        inconsistency by accessing the Node's state.
             Returns the manhattan distance --> an int
         """
+        if isinstance(state, Node):
+            state = state.state
         x1, y1 = self.coordinates
         x2, y2 = state.coordinates
         return (abs(x1 - x2) + abs(y1 - y2))
-    
-
 
 
 class Node():
@@ -59,6 +58,17 @@ class Node():
         self.successors = []
 
     
+    def generate_path(self):
+        """Recursively works its way down the Node's ancestors to
+        the start state, aggregating the coordinates of each node
+        into a list.
+        """
+        if self.parent is not None:
+            return self.parent.generate_path() + [self.state.coordinates]
+        else:
+            return [self.state.coordinates]
+
+
     def generate_path_cost(self):
         """Recursively works its way down the Node's ancestors to
         the start state, aggregating the cost of each node.
@@ -91,17 +101,6 @@ class Node():
                     parent=self,
                     cost=1)
                 self.successors.append(node)
-
-
-    def generate_path(self):
-        """Recursively works its way down the Node's ancestors to
-        the start state, aggregating the coordinates of each node
-        into a list.
-        """
-        if self.parent is not None:
-            return self.parent.generate_path() + [self.state.coordinates]
-        else:
-            return [self.state.coordinates]
 
 
     def manhattan_distance_from(self, state):
@@ -158,97 +157,34 @@ class Search():
         self.start_state = None
         self.goal_state = None
         self.search_file = search_file
+        # Used to see if search_file has changed
+        self._old_search_file = None
         self.state_space = None
 
-
-    def generate_state_space(self, search_file=None):
-        """Iterate through the search_file, recording the States
-        along with the start and goal States. The file is broken
-        up into a 2D array, thereby determining the coordinates
-        of the generated States by using the row and column
-        numbers.
-            Returns the state_space dictionary.
+    def _a_star_search(self):
+        """Pick the node on the frontier with the lowest evaluation
+        function result.
+        The heuristic used is the Manhattan distance from goal
+        The evaluation function = heuristic result + path cost
+            Returns the next node to be expanded
         """
-        if search_file == None:
-            search_file = self.search_file
-        state_space = {}
-        f = open(search_file, 'r')
-        array = f.read().splitlines()
-        f.close()
-        # Break file text into 2D array of characters
-        for line in range(len(array)):
-            array[line] = list(array[line])
-        # Keep the array for use in printing out solution later
-        self.maze_array = array
-        # Create states from 2D array
-        for row in range(len(array)):
-            for col in range(len(array[row])):
-                cell_text = array[row][col]
-                if cell_text not in self.wall_symbol:
-                    current_state = State((row, col), cell_text)
-                    if cell_text in self.start_state_symbol:
-                        self.start_state = current_state
-                    if cell_text in self.goal_state_symbol:
-                        self.goal_state = current_state
-                    state_space[(row, col)] = current_state
-        return state_space
-
-
-    def already_in_frontier(self, node, frontier=None):
-        """A Node is given as an argument. Determines if the Node's
-        State is currently in the frontier.
-        If the Node's state is already in the frontier, the path cost
-        of each Node is compared, and the Node in the frontier is
-        replaced if its path cost is greater than the path cost of
-        the passed Node.
-        NOTE: The passed Node will replace the old Node at the same
-        index for this implementation.
-            return True if the node is already in the frontier,
-            Else return False
-        """
-        if frontier == None:
-            frontier = self.frontier
-        if frontier == []:
-            return False
+        frontier = self.frontier
+        next_node = frontier[0]
+        index_to_pop = None
         for index in range(len(frontier)):
-            frontier_node = frontier[index]
-            if frontier_node.state == node.state:
-                if frontier_node.generate_path_cost() \
-                        < node.generate_path_cost():
-                    # TODO remove old node and insert new node based on path cost?
-                    frontier[index] = node
-                return True
-        return False
-
-
-    def already_visited(self, node, visited_states=None):
-        """A Node is given as an argument. Determines if the Node's
-        State has been visited by the Search.
-            return True if the node has already been expanded,
-            Else returm false
-        """
-        if visited_states == None:
-            visited_states = self.visited_states
-        if node.state in visited_states:
-                return True
-        return False
-
-
-    def node_is_valid(self, node):
-        """A Node is given as an argument. Determines if the Node
-        is valid by comparing its state with the states in the
-        visited nodes list and the frontier list.
-            return True if Node is not expanded or on the frontier,
-            Else return False
-        """
-        # Check if node's state already exists in visited nodes.
-        # Check if node's state already exists in frontier.
-        if (not self.already_visited(node)):
-            if (not self.already_in_frontier(node)):
-                return True
-        return False
-
-
+            node = frontier[index]
+            next_node_evaluation = self.evaluate(next_node)
+            node_evaluation = self.evaluate(node)
+            if (node_evaluation < \
+                    next_node_evaluation):
+                next_node = node
+                index_to_pop = index
+        if index_to_pop is None:
+            index_to_pop = 0
+        next_node = frontier.pop(index_to_pop)
+        return next_node
+ 
+ 
     def _breadth_first_search(self):
         """Expands the shallowest unexpanded node.
         Implemented with frontier as FIFO queue.
@@ -289,6 +225,47 @@ class Search():
         return next_node
 
 
+    # TODO move to Node class? Rename to 'is_already_in_the_frontier'?
+    def already_in_frontier(self, node, frontier=None):
+        """A Node is given as an argument. Determines if the Node's
+        State is currently in the frontier.
+        If the Node's state is already in the frontier, the path cost
+        of each Node is compared, and the Node in the frontier is
+        replaced if its path cost is greater than the path cost of
+        the passed Node.
+        NOTE: The passed Node will replace the old Node at the same
+        index for this implementation.
+            return True if the node is already in the frontier,
+            Else return False
+        """
+        if frontier == None:
+            frontier = self.frontier
+        if frontier == []:
+            return False
+        for index in range(len(frontier)):
+            frontier_node = frontier[index]
+            if frontier_node.state == node.state:
+                if frontier_node.generate_path_cost() \
+                        < node.generate_path_cost():
+                    # TODO remove old node and insert new node based on path cost?
+                    frontier[index] = node
+                return True
+        return False
+
+    # TODO move to Node class? rename to 'has_already_been_visited'?
+    def already_visited(self, node, visited_states=None):
+        """A Node is given as an argument. Determines if the Node's
+        State has been visited by the Search.
+            return True if the node has already been expanded,
+            Else returm false
+        """
+        if visited_states == None:
+            visited_states = self.visited_states
+        if node.state in visited_states:
+                return True
+        return False
+
+    # TODO move to Node class? provide additional argument or setting to decide heuristic?
     def evaluate(self, node):
         """Determines the Node's A* algorithm evaluation function.
         Utilizes the manhattan distance.
@@ -300,30 +277,112 @@ class Search():
         return man_dist + path_cost
 
 
-    def _a_star_search(self):
-        """Pick the node on the frontier with the lowest evaluation
-        function result.
-        The heuristic used is the Manhattan distance from goal
-        The evaluation function = heuristic result + path cost
-            Returns the next node to be expanded
+    def generate_maze_solution(self, path):
+        """Uses the maze array to build the string printed to stdout.
         """
-        frontier = self.frontier
-        next_node = frontier[0]
-        index_to_pop = None
-        for index in range(len(frontier)):
-            node = frontier[index]
-            next_node_evaluation = self.evaluate(next_node)
-            node_evaluation = self.evaluate(node)
-            if (node_evaluation < \
-                    next_node_evaluation):
-                next_node = node
-                index_to_pop = index
-        if index_to_pop is None:
-            index_to_pop = 0
-        next_node = frontier.pop(index_to_pop)
-        return next_node
-    
-        
+        visited = self.visited_states
+        # Possibly configurable values
+        path_state_symbol = '.'
+        traversed_state_symbol = '0'
+        #expanded_state_symbol = 'X'
+        ##############################
+        array = copy.deepcopy(self.maze_array)
+        array_string = ''
+        if SHOW_VISITED == True:
+            for state in visited:
+                x, y = state.coordinates
+                array[x][y] = traversed_state_symbol
+        for coordinate in path:
+            x, y = coordinate
+            array[x][y] = path_state_symbol
+        for row in range(len(array)):
+            for col in range(len(array[row])):
+                array_string += array[row][col]
+            array_string += '\n'
+        return array_string
+
+
+    def generate_solutions_dict(self, solution_node):
+        """Calls the necessary functions to generate the solution
+        dictionary. Takes the solution Node as an argument.
+            Returns the solutions dictionary.
+        """
+        solutions = {}
+        path = solution_node.generate_path()
+        solutions['Maze Solution'] = self.generate_maze_solution(path)
+        solutions['Path Cost'] = solution_node.generate_path_cost()
+        solutions['Expanded Node Count'] = self.count_of_expanded_nodes
+        return solutions
+
+
+    def generate_state_space(self, search_file=None):
+        """Iterate through the search_file, recording the States
+        along with the start and goal States. The file is broken
+        up into a 2D array, thereby determining the coordinates
+        of the generated States by using the row and column
+        numbers.
+            Returns the state_space dictionary.
+        """
+        if (search_file == None) and (self.search_file != None):
+            search_file = self.search_file
+        else:
+            raise Exception("Invalid search file '%s'" %(search_file))
+        # Save execution time if search file has not been changed
+        if (search_file == self._old_search_file):
+            return self.state_space
+        state_space = {}
+        f = open(search_file, 'r')
+        array = f.read().splitlines()
+        f.close()
+        # Break file text into 2D array of characters
+        for line in range(len(array)):
+            array[line] = list(array[line])
+        # Keep the array for use in printing out solution later
+        self.maze_array = array
+        # Create states from 2D array
+        for row in range(len(array)):
+            for col in range(len(array[row])):
+                cell_text = array[row][col]
+                if cell_text not in self.wall_symbol:
+                    current_state = State((row, col), cell_text)
+                    if cell_text in self.start_state_symbol:
+                        self.start_state = current_state
+                    if cell_text in self.goal_state_symbol:
+                        self.goal_state = current_state
+                    state_space[(row, col)] = current_state
+        return state_space
+
+    # TODO move to Node class? Rename to 'is_valid'? this_node.is_valid?
+    def node_is_valid(self, node):
+        """A Node is given as an argument. Determines if the Node
+        is valid by comparing its state with the states in the
+        visited nodes list and the frontier list.
+            return True if Node is not expanded or on the frontier,
+            Else return False
+        """
+        # Check if node's state already exists in visited nodes.
+        # Check if node's state already exists in frontier.
+        if (not self.already_visited(node)):
+            if (not self.already_in_frontier(node)):
+                return True
+        return False
+
+
+    def print_solutions_dict(self, search_name, solutions):
+        """Iterates through the keys of the solutions dictionary
+        and prints the values in a readable format to stdout.
+        Each solution is printed out with a lable indicating the
+        search algorithm used.
+        """
+        search_name = search_name.upper()
+        # Print the search label
+        print "########## %s SEARCH RESULTS for '%s' ##########" \
+                %(search_name, self.search_file)
+        # Print the solutions dictionary
+        for key in solutions.keys():
+            print "%25s:\n%s" %(key, solutions[key])
+
+
     def search(self, search_name):
         """The main driver of the program. Generates needed data for
         the search then calls the necessary functions to aggregate and
@@ -385,55 +444,4 @@ class Search():
             self.visited_states = []
             self.count_of_expanded_nodes = 0
 
-
-    def generate_solutions_dict(self, solution_node):
-        """Calls the necessary functions to generate the solution
-        dictionary. Takes the solution Node as an argument.
-            Returns the solutions dictionary.
-        """
-        solutions = {}
-        path = solution_node.generate_path()
-        solutions['Maze Solution'] = self.generate_maze_solution(path)
-        solutions['Path Cost'] = solution_node.generate_path_cost()
-        solutions['Expanded Node Count'] = self.count_of_expanded_nodes
-        return solutions
-
-    def generate_maze_solution(self, path):
-        """Uses the maze array to build the string printed to stdout.
-        """
-        visited = self.visited_states
-        # Possibly configurable values
-        path_state_symbol = '.'
-        traversed_state_symbol = '0'
-        #expanded_state_symbol = 'X'
-        ##############################
-        array = copy.deepcopy(self.maze_array)
-        array_string = ''
-        if SHOW_VISITED == True:
-            for state in visited:
-                x, y = state.coordinates
-                array[x][y] = traversed_state_symbol
-        for coordinate in path:
-            x, y = coordinate
-            array[x][y] = path_state_symbol
-        for row in range(len(array)):
-            for col in range(len(array[row])):
-                array_string += array[row][col]
-            array_string += '\n'
-        return array_string
-
-
-    def print_solutions_dict(self, search_name, solutions):
-        """Iterates through the keys of the solutions dictionary
-        and prints the values in a readable format to stdout.
-        Each solution is printed out with a lable indicating the
-        search algorithm used.
-        """
-        search_name = search_name.upper()
-        # Print the search label
-        print "########## %s SEARCH RESULTS for '%s' ##########" \
-                %(search_name, self.search_file)
-        # Print the solutions dictionary
-        for key in solutions.keys():
-            print "%25s:\n%s" %(key, solutions[key])
 
