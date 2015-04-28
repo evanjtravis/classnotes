da38a46a0353511a5d2babd527e9e63e250fbe8b
Evan J. Travis <ejtravis@illinois.edu>
2015-04-28 16:31:52 -0500

    Rubbish.

--- courses/cs440/hw/hw_4/gridworld/gridworld.py
+++ courses/cs440/hw/hw_4/gridworld/gridworld.py
@@ -18,7 +18,7 @@ BASE_REWARD = -0.04
 ERR_C =        0.1
 
 # Discount factor
-Y =       0.99
+Y =       0.9
 P_CHILD = 0.8
 # Perpendicular
 P_PERP =  0.1
@@ -39,6 +39,7 @@ ACTION_COORDS = {
     LEFT:  (-1, 0)
 }
 ACTION_CHARS = {
+    None:  '.',
     UP:    '^',
     DOWN:  'v',
     LEFT:  '<',
@@ -83,13 +84,50 @@ class State(object):
         self.char = char
         self.coords = coords
         self.is_terminal, self.reward = self.process_char(char)
+
         self.utility = (0.0, None)
+        #if self.is_terminal:
+        #    self.utility = (self.reward, None)
         self.actions = {
-            UP:    BASE_REWARD, # TO BE FLOATS
-            DOWN:  BASE_REWARD,
-            LEFT:  BASE_REWARD,
-            RIGHT: BASE_REWARD
+            UP:    0.0,
+            DOWN:  0.0,
+            LEFT:  0.0,
+            RIGHT: 0.0
+
         }
+
+    def __str__(self):
+        """c
+        """
+        print_var = None
+        if self.is_terminal:
+            print_var = self.char
+        elif self.is_invalid():
+            print_var = self.char
+        else:
+            action = self.utility[1]
+            print_var = ACTION_CHARS[action]
+        return "| %s " %(print_var)
+
+    def set_max_utility(self):
+        """
+        """
+        max_utility = None
+        max_key = None
+        actions = self.actions
+        for key in actions:
+            if actions[key] > max_utility:
+                max_utility = actions[key]
+                max_key = key
+        self.utility = (max_utility, max_key)
+
+    def is_invalid(self):
+        """c
+        """
+        invalid = False
+        if self.char == self.WALL:
+            invalid = True
+        return invalid
 
     def get_children_coords(self):
         """c
@@ -147,17 +185,21 @@ class State(object):
         return Diff
 
 
-    def is_child_of(self, state, action):
-        """Is given state a parent based from previous action?
-        """
-        return state.has_child(self, action)
-
-    def is_parent_of(self, state, action):
+    def is_parent_of(self, state, action, map_cells):
         """Is given state a child based from the action?
         """
         is_parent = False
-        if(self.add(action.coords) == state.coords):
+        new_coords = self.add(action.coords)
+        if(new_coords == state.coords):
             is_parent = True
+        elif new_coords != None:
+            i, j = new_coords
+            try:
+                next_state = map_cells[i][j]
+                if next_state.is_invalid():
+                    is_parent = True
+            except IndexError:
+                is_parent = True
         return is_parent
 
     def is_adjacent_to(self, state):
@@ -174,12 +216,14 @@ class State(object):
     def is_perpendicular_to(self, state, action):
         """c
         """
-        is_perpendicular = True
+        is_perpendicular = False
         if state.is_adjacent_to(self):
             a_diff = self.diff(action)
             s_diff = self.diff(state)
             if add_coords(a_diff, s_diff) == (0, 0):
-                is_perpendicular = False
+                is_perpendicular = True
+            elif s_diff == (0,0):
+                is_perpendicular = True
         return is_perpendicular
 
 
@@ -190,8 +234,8 @@ class Agent(object):
         """c
         """
         self.mapfile = mapfile
-        self.map_cells, self.start_coords = self.read_map(mapfile)
-        START = self.map_cells[0][0].START
+        self.map_cells, self.start_coords, START =\
+            self.read_map(mapfile)
         if None in self.start_coords:
             raise Exception(
                 "No start character '%s' found in mapfile '%s'."%\
@@ -204,14 +248,23 @@ class Agent(object):
         array = f.read().splitlines()
         f.close()
         start_coords = (None, None)
+        start_char = State.START
 
         for i in range(len(array)):
             array[i] = list(array[i])
             for j in range(len(array[i])):
                 char = array[i][j]
+                if char == start_char:
+                    if start_coords == (None, None):
+                        start_coords = (i, j)
+                    else:
+                        raise Exception(
+                            "More than one starting character '%s'\
+found in mapfile '%s'" %(start_char, mapfile)
+                        )
                 coords = (i, j)
                 array[i][j] = State(char, coords)
-        return array, start_coords
+        return array, start_coords, start_char
 
 
 
@@ -226,8 +279,14 @@ class Agent(object):
     def coords_are_valid(self, coords):
         """c
         """
-        x, y = coords
-        valid = self.coord_is_valid(x) and self.coord_is_valid(y)
+        valid = False
+        if coords != None:
+            x, y = coords
+            if self.coord_is_valid(x) and self.coord_is_valid(y):
+                valid = True
+                state = self.map_cells[x][y]
+                if state.is_invalid():
+                    valid = False
         return valid
 
 
@@ -239,7 +298,7 @@ class Agent(object):
         r_max = R_MAX
         e = c * r_max
         y = Y
-        delta = MININT
+        delta = MAXINT
         min_delta = (e * ((1.0 - y) / y))
         iterations = 0
         while (delta >= min_delta) and (iterations < MAX_ITERATIONS):
@@ -248,17 +307,24 @@ class Agent(object):
             for i in range(len(states)):
                 for j in range(len(states[i])):
                     delta = self.U(i, j, delta)
-        return states
+            for i in range(len(states)):
+                for j in range(len(states[i])):
+                    state = states[i][j]
+                    state.set_max_utility()
 
 
     def U(self, i, j, delta=0.0):
         """c
         """
         state = self.map_cells[i][j]
-        if state.is_terminal:
+        if state.is_invalid():
             return delta
+        #if state.is_terminal:
+        #    return delta
         reward = R(state)
-        old_utility = state.action[0]
+        old_utility = state.utility[0]
+        if old_utility == None:
+            old_utility = MININT
         children_coords = state.get_children_coords()
         actions = state.actions
         for action_key in actions:
@@ -266,30 +332,91 @@ class Agent(object):
             ACTION = ACTIONS[action_key]
             for child_coords in children_coords:
                 if not self.coords_are_valid(child_coords):
-                    q_value += BASE_REWARD
+                    i, j = state.coords
                 else:
                     i, j = child_coords
-                    child = self.map_cells[i][j]
-                    p_of_child = P(child, state, ACTION)
-                    child_utility = child.action[0]
+                child = self.map_cells[i][j]
+                if child.is_invalid():
+                    child = state
+                p_of_child = P(child, state, ACTION, self.map_cells)
+                child_utility = child.utility[0]
+                if child_utility == None:
+                    child_utility = MININT
+                else:
                     q_value += (p_of_child * child_utility)
-            q_value = reward + (q_value * Y)
+            q_value = reward + (Y * q_value)
             state.actions[action_key] = q_value
-            if q_value > old_utility:
-                state.action = (q_value, action_key)
             utility_diff = abs(q_value - old_utility)
             if utility_diff > delta:
                 delta = utility_diff
-        self.map_cells[i][j] = state
         return delta
 
 
+    def print_mdp_solution_map(self):
+        """c
+        """
+        msg = ''
+        msg_lines = []
+        filler_lines = []
+        max_len = 0
+        states = self.map_cells
+        for i in range(len(states)):
+            msg_line = ''
+            filler_line = ''
+            for j in range(len(states[i])):
+                state = states[i][j]
+                nxt_str = str(state)
+                msg_line += nxt_str
+                padding = len(nxt_str) - 1
+                filler_line += "|%s" %(' ' * padding)
+            max_len = max(max_len, len(msg_line))
+            msg_line += '|\n'
+            msg_lines.append(msg_line)
+            filler_line = filler_line + '|\n'
+            filler_lines.append(filler_line)
+        top_bottom_border = '-' + ('-' * max_len) + '\n'
+        msg += top_bottom_border
+        for i in range(len(msg_lines)):
+            line = msg_lines[i]
+            filler = filler_lines[i]
+            msg += filler
+            msg += line
+            msg += filler
+            msg += top_bottom_border
+        print msg
 
-def P(state, parent_state, action):
+
+    def print_mdp_solution_utility_values(self):
+        """c
+        """
+        states = self.map_cells
+        msg = ''
+        for i in range(len(states)):
+            for j in range(len(states[i])):
+                state = states[i][j]
+                if state.is_invalid():
+                    continue
+                utility = state.utility[0]
+                msg += "(%d,%d): %-.6f\n" %\
+                    (j, i, utility)
+        print msg
+
+
+    def print_mdp_solution(self):
+        """c
+        """
+        self.print_mdp_solution_utility_values()
+        self.print_mdp_solution_map()
+
+
+#=====================================================================
+# Utils
+#---------------------------------------------------------------------
+def P(state, parent_state, action, map_cells):
     """Probability of moving from prev_state to state.
     """
     probs = 0.0
-    if parent_state.is_parent_of(state, action):
+    if parent_state.is_parent_of(state, action, map_cells):
         probs = P_CHILD
     elif parent_state.is_perpendicular_to(state, action):
         probs = P_PERP
@@ -311,3 +438,25 @@ def add_coords(coord_1, coord_2):
     a = i + x
     b = j + y
     return (a, b)
+#=====================================================================
+
+#=====================================================================
+# Program Execution
+#---------------------------------------------------------------------
+def demo():
+    """c
+    """
+    mapfile = "map"
+    agent = Agent(mapfile)
+    agent.value_iteration_method()
+    agent.print_mdp_solution()
+    import pdb; pdb.set_trace()
+
+
+
+#=====================================================================
+
+
+
+if __name__ == "__main__":
+    demo()
